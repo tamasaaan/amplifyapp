01b6c5b9f1a85cae454cc510eaf36417c37beed7
tamasaaan <getchoo_oh_oh@yahoo.co.jp>
2020-12-04 15:05:25 +0900

    add aws-exports.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
@@ -21,10 +21,9 @@ function App() {
           Learn React amplify test
         </a>
       </header>
-      {/*<AmplifySignOut />*/}
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
-// export default withAuthenticator(App);
+export default withAuthenticator(App);
